95270526c60757f0cba9f450c6bef361df252fff
JustSam <samkamoheloratlou.final@gmail.com>
2025-01-12 23:52:51 +0200

    user Template-driven form done, Task Reactive form in dev and almost done, UI still need to be worked on

--- src/app/components/content-view/forms/newTask-form/new-task-from.component.ts
+++ src/app/components/content-view/forms/newTask-form/new-task-from.component.ts
@@ -1,30 +1,69 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { EmailValidator, FormControl, FormsModule, Validators } from '@angular/forms';
+
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
+
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-new-task-from',
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './new-task-from.component.html',
 })
-export class NewTaskFromComponent {
+export class NewTaskFromComponent implements OnInit {
+  //create form group
+  taskForm!: FormGroup;
 
-  formData = {
-    name: '',
-    email: '',
-    message: ''
-  };
+  severities = [
+    { value: 1, label: "low"},
+    { value: 2, label: "medium"},
+    { value: 3, label: "high"}
+  ];
 
-  validateName(name: string): boolean{
-    return false;
+  constructor(private tb: FormBuilder){}
+
+  ngOnInit(){
+
+    //making use of FormBuilder
+    this.taskForm = this.tb.group({
+      taskName: [
+        '',
+        Validators.required,
+        Validators.minLength(1)
+      ],
+      taskMessage: [
+        '',
+        Validators.required,
+        Validators.maxLength(100)
+      ],
+      severity: ['', Validators.required]
+    });
+
+    //Listen for changes
+    this.taskForm.valueChanges.subscribe(value => {
+      console.log("Form value changed: ", value);
+    })
   }
 
-  validateEmail(name: EmailValidator): boolean{
-    return false;
+  onSubmit(){
+    if(this.taskForm?.valid){
+      console.log("this.taskForm.value");
+      //process of form data to be added
+    }
+    else{
+      this.markFormGroupTouched(this.taskForm!);
+    }
   }
 
+  markFormGroupTouched(formGroup: FormGroup){
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
 
-  onSubmit() {
-    console.log('Form submitted:', this.formData);
+      if(control instanceof FormGroup){
+        this.markFormGroupTouched(control);
+      }
+    })
   }
+
 }
--- src/app/components/content-view/forms/user-form/user-form.component.ts
+++ src/app/components/content-view/forms/user-form/user-form.component.ts
@@ -27,7 +27,7 @@ export class UserFormComponent{
 
   constructor(private userService: UserServices){}
 
-  onSubmit(){
+  onSubmit(form:NgForm){
 
 
 
